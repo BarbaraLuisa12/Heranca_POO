a4c30ffd3199cba55486390b78326aeb92a98fd2
Barbara Ribeiro <61757270+BarbaraLuisa12@users.noreply.github.com>
2024-06-05 20:31:21 -0300

    Delete Heranca directory

--- Heranca/Estagiario.cpp
+++
@@ -1,8 +0,0 @@
-#include "Estagiario.hpp"
-
-Estagiario::Estagiario(string nome, int id, double salarioBase):
-    Funcionario(nome, id, salarioBase){}
-
-double Estagiario::calcularSalarioTotal() const {
-    return salarioBase * 0.5;
-}
